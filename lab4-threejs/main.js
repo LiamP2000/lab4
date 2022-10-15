ec485152c196d8e6e8712750ef7fe27a9e1c57d5
LiamP2000 <liampeeters@live.be>
2022-10-15 15:38:21 +0200

    fix: classes and camera position

--- lab4-threejs/main.js
+++ lab4-threejs/main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { TextureLoader } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Environment from '/src/environment.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -13,18 +14,17 @@ document.body.appendChild( renderer.domElement );
 // add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const environment = new THREE.BoxGeometry( 50, 50, 50 );
-const material = new THREE.MeshBasicMaterial( { color: 0x87ceeb } );
-const cube = new THREE.Mesh( environment, material );
-material.side = THREE.BackSide;
-scene.add( cube );
+// add environment
+const environment = new Environment();
+scene.add(environment.group);
 
-camera.position.z = 5;
+camera.position.set( 5, 10, 80 );
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	camera.lookAt(cube.position);
+	//camera.lookAt(cube.position);
 	renderer.render( scene, camera );
 };
 
