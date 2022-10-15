673466f68a3538777376d7f4a3a5608f8176ecc9
LiamP2000 <liampeeters@live.be>
2022-10-15 14:41:44 +0200

    feat: created cube environment

--- lab4-threejs/main.js
+++ lab4-threejs/main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { TextureLoader } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,9 +10,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+// add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const environment = new THREE.BoxGeometry( 50, 50, 50 );
+const material = new THREE.MeshBasicMaterial( { color: 0x87ceeb } );
+const cube = new THREE.Mesh( environment, material );
+material.side = THREE.BackSide;
 scene.add( cube );
 
 camera.position.z = 5;
@@ -18,9 +24,7 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	camera.lookAt(cube.position);
 	renderer.render( scene, camera );
 };
 
