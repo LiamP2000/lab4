a25abc7ef5c89ec46952f1d0c36473ff736acade
LiamP2000 <liampeeters@live.be>
2022-10-15 15:57:53 +0200

    feat: change function names

--- lab4-threejs/src/environment.js
+++ lab4-threejs/src/environment.js
@@ -3,12 +3,12 @@ import * as THREE from 'three';
 export default class Environment {
     constructor() {
         this.group = new THREE.Group();
-        this.createEnvironment();
-        this.createFloor();
+        this.addEnvironment();
+        this.addFloor();
 
     }
 
-    createEnvironment() {
+    addEnvironment() {
         const cube = new THREE.BoxGeometry( 200, 200, 200 );
         const material = new THREE.MeshBasicMaterial( { color: 0x87ceeb } );
         material.side = THREE.BackSide;
@@ -16,9 +16,9 @@ export default class Environment {
         this.group.add(environment);
     }
 
-    createFloor() {
+    addFloor() {
         const geometry = new THREE.BoxGeometry( 60, 1, 60 );
-        const material = new THREE.MeshStandardMaterial( { color: 0x009a17 } );
+        const material = new THREE.MeshLambertMaterial( { color: 0x009a17 } );
         material.side = THREE.BackSide;
         const floor = new THREE.Mesh( geometry, material );
         this.group.add(floor);
