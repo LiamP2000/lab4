e8f321b0b2af0091d7db83d9a5e4f93adc7012cb
LiamP2000 <liampeeters@live.be>
2022-10-16 12:14:18 +0200

    feat: class House with walls and roof

---
+++ lab4-threejs/src/house.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+
+export default class House {
+    constructor() {
+        this.group = new THREE.Group();
+        this.addWalls();
+        this.addRoof();
+    }
+
+    addWalls() {
+        const textureLoader = new THREE.TextureLoader();
+        const bricks = textureLoader.load('/textures/bricks.jpg');
+
+        const  geometry = new THREE.BoxGeometry( 20, 15, 1 );
+        const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+        material.map = bricks;
+        const wall1 = new THREE.Mesh( geometry, material );
+        const wall2 = new THREE.Mesh( geometry, material );
+        const wall3 = new THREE.Mesh( geometry, material );
+        const wall4 = new THREE.Mesh( geometry, material );
+
+        wall1.position.set( 0, 8, 10 );
+        wall2.position.set( 0, 8, -10 );
+        wall3.position.set( 10, 8, 0 );
+        wall3.rotation.y = Math.PI / 2;
+        wall4.position.set( -10, 8, 0 );
+        wall4.rotation.y = Math.PI / 2;
+
+        this.group.add(wall1);
+        this.group.add(wall2);
+        this.group.add(wall3);
+        this.group.add(wall4);
+        
+    }
+
+    addRoof() {
+        const textureLoader = new THREE.TextureLoader();
+        const roofPanels = textureLoader.load('/textures/roof.jpg');
+        const geometry = new THREE.ConeGeometry( 17, 10, 4 );
+        const material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+        material.map = roofPanels;
+        const roof = new THREE.Mesh( geometry, material );
+        
+
+        roof.position.set( 0, 20, 0 );
+        roof.rotation.y = Math.PI / 4;
+        this.group.add(roof);
+
+    }
+}
