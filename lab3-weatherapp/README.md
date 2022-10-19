f7225f63772f4a9319c4ea76a26a05504665ed28
LiamP2000 <77250649+LiamP2000@users.noreply.github.com>
2022-10-19 11:20:01 +0200

    Delete lab3-weatherapp directory

--- lab3-weatherapp/README.md
+++
@@ -1,2 +0,0 @@
-# Lab 3
-Codesandbox link: https://codesandbox.io/s/condescending-chaum-spe0jk?file=/dist/assets/index.49eb995f.js
--- lab3-weatherapp/main.js
+++
@@ -1,4 +0,0 @@
-import './style.css'
-import Weather from './src/Weather.js'
-
-const weather = new Weather("1729c3bfaa00451b88363217220510");
